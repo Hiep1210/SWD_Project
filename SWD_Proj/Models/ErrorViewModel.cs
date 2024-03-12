5f1606cc426a2bbaec0a0d06004fc604728fd310
Hiep1210 <nguyenhoanghiep1210003@gmail.com>
2024-03-13 00:00:29 +0700

    first

---
+++ SWD_Proj/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SWD_Proj.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
